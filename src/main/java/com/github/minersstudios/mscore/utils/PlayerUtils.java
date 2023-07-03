6f588ffd37d533b44c7dc1a3e9618c2ec04a5d55
Poloskun_ <nikongricisin339@gmail.com>
2023-07-04 01:07:56 +0300

    some improvements

--- src/main/java/com/github/minersstudios/mscore/utils/PlayerUtils.java
+++ src/main/java/com/github/minersstudios/mscore/utils/PlayerUtils.java
@@ -4,16 +4,23 @@ import com.github.minersstudios.mscore.MSCore;
 import com.github.minersstudios.msessentials.player.PlayerInfo;
 import com.google.common.base.Charsets;
 import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import com.mojang.authlib.properties.PropertyMap;
 import net.kyori.adventure.text.Component;
+import net.minecraft.network.protocol.game.*;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.ServerPlayerGameMode;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.BiomeManager;
 import org.apache.commons.io.IOUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.craftbukkit.v1_20_R1.CraftServer;
+import org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
@@ -23,8 +30,7 @@ import org.json.simple.JSONValue;
 
 import java.net.URL;
 import java.nio.charset.Charset;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 public final class PlayerUtils {
     public static final @NotNull String UUID_URL = "https://api.mojang.com/users/profiles/minecraft/";
@@ -115,6 +121,102 @@ public final class PlayerUtils {
 
         online.loadData();
         return online;
+    }
+
+    /**
+     * Sets player's skin with specified value and signature.
+     * Use {@link PlayerUtils#reloadPlayer(Player)} to reload the player
+     * and make the skin visible.
+     *
+     * @param player    Player whose skin will be set
+     * @param value     Value of the skin
+     * @param signature Signature of the skin
+     */
+    public static void setSkin(
+            @NotNull Player player,
+            @NotNull String value,
+            @NotNull String signature
+    ) {
+        CraftPlayer craftPlayer = (CraftPlayer) player;
+        GameProfile gameProfile = craftPlayer.getProfile();
+        PropertyMap propertyMap = gameProfile.getProperties();
+        Property newProperty = new Property("textures", value, signature);
+
+        if (propertyMap.containsKey("textures")) {
+            Property oldProperty = propertyMap.get("textures").iterator().next();
+            propertyMap.remove("textures", oldProperty);
+        }
+
+        propertyMap.put("textures", newProperty);
+    }
+
+    /**
+     * Reloads player, like if he was joining the server
+     *
+     * @param player Player whose will be reloaded
+     */
+    public static void reloadPlayer(@NotNull Player player) {
+        MSCore plugin = MSCore.getInstance();
+        Location location = player.getLocation();
+        CraftPlayer craftPlayer = (CraftPlayer) player;
+        ServerPlayer serverPlayer = craftPlayer.getHandle();
+        ServerGamePacketListenerImpl playerConnection = serverPlayer.connection;
+        ServerPlayerGameMode gameMode = serverPlayer.gameMode;
+        ServerLevel level = serverPlayer.serverLevel();
+
+        ClientboundPlayerInfoRemovePacket removePacket = new ClientboundPlayerInfoRemovePacket(
+                List.of(player.getUniqueId())
+        );
+        ClientboundPlayerInfoUpdatePacket addPacket = new ClientboundPlayerInfoUpdatePacket(
+                ClientboundPlayerInfoUpdatePacket.Action.ADD_PLAYER,
+                serverPlayer
+        );
+        ClientboundRespawnPacket respawnPacket = new ClientboundRespawnPacket(
+                level.dimensionTypeId(),
+                level.dimension(),
+                BiomeManager.obfuscateSeed(level.getSeed()),
+                gameMode.getGameModeForPlayer(),
+                gameMode.getPreviousGameModeForPlayer(),
+                level.isDebug(),
+                level.isFlat(),
+                (byte) 0xFF,
+                serverPlayer.getLastDeathLocation(),
+                serverPlayer.portalCooldown
+        );
+        ClientboundPlayerPositionPacket positionPacket = new ClientboundPlayerPositionPacket(
+                location.getX(),
+                location.getY(),
+                location.getZ(),
+                location.getYaw(),
+                location.getPitch(),
+                Collections.emptySet(),
+                0
+        );
+        ClientboundSetCarriedItemPacket heldItemPacket = new ClientboundSetCarriedItemPacket(
+                player.getInventory().getHeldItemSlot()
+        );
+
+        Bukkit.getOnlinePlayers().forEach(onlinePlayer -> {
+            if (onlinePlayer.equals(player)) return;
+            onlinePlayer.hidePlayer(plugin, player);
+            onlinePlayer.showPlayer(plugin, player);
+        });
+
+        playerConnection.send(removePacket);
+        playerConnection.send(addPacket);
+        playerConnection.send(respawnPacket);
+        playerConnection.send(positionPacket);
+        playerConnection.send(heldItemPacket);
+
+        craftPlayer.updateScaledHealth();
+        serverPlayer.containerMenu.sendAllDataToRemote();
+
+        if (player.isOp()) {
+           Bukkit.getScheduler().runTask(plugin, () -> {
+               player.setOp(false);
+               player.setOp(true);
+           });
+        }
     }
 
     /**
